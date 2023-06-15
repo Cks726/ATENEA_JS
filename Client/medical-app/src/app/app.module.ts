e1f7fe8aabf4092da50c7f3c858f5c6a98c040b1
naty <natymayorn@gmail.com>
2023-06-15 00:26:33 -0500

    "ok"

--- Client/medical-app/src/app/app.module.ts
+++ Client/medical-app/src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PacientesComponent } from './pacientes/pacientes.component';
+import { RouterModule, Routes } from '@angular/router';
+import { DoctoresComponent } from './doctores/doctores.component';
+
+const routes: Routes = [
+  {path: '',redirectTo: '/home', pathMatch: 'full'},
+  {path: 'pacientes', component: PacientesComponent},
+  {path: 'doctores', component: DoctoresComponent},
+  
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PacientesComponent,
+    DoctoresComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
